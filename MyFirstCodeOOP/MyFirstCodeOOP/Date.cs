aac8d65e65bb6b324a4d0c5cc782a655641bf5d0
Carlos Diaz <junior.diaz@selazar.com>
2023-02-14 22:07:38 -0500

    Properties created

---
+++ MyFirstCodeOOP/MyFirstCodeOOP/Date.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstCodeOOP
+{
+    public class Date
+    {
+        private int _year;
+        private int _month;
+        private int _day;
+    }
+}
