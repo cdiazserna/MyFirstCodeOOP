03ce17a40b76cbd5c2fa6fed4e3c4cf0e7621cd1
Carlos Diaz <junior.diaz@selazar.com>
2023-02-15 16:41:05 -0500

    New SalaryEmployee.cs created

--- MyFirstCodeOOP/MyFirstCodeOOP/Program.cs
+++ MyFirstCodeOOP/MyFirstCodeOOP/Program.cs
@@ -8,20 +8,33 @@ namespace MyFirstCodeOOP
         {
             try
             {
-                //Date dateObject = new Date(2023, 2, 29);
-                Console.WriteLine("OOP CONCEPTS");
-                Console.WriteLine("************");
+                ////Date dateObject = new Date(2023, 2, 29);
+                //Console.WriteLine("OOP CONCEPTS");
+                //Console.WriteLine("************");
 
-                Console.WriteLine("Please put your year:");
-                var year = Console.ReadLine();
-                Console.WriteLine("");
-                Console.WriteLine("Please put your month:");
-                var month = Console.ReadLine();
-                Console.WriteLine("");
-                Console.WriteLine("Please put your day:");
-                var day = Console.ReadLine();
-                Console.WriteLine("");
-                Console.WriteLine(new Date(Convert.ToInt32(year), Convert.ToInt32(month), Convert.ToInt32(day)));
+                //Console.WriteLine("Please put your year:");
+                //var year = Console.ReadLine();
+                //Console.WriteLine("");
+                //Console.WriteLine("Please put your month:");
+                //var month = Console.ReadLine();
+                //Console.WriteLine("");
+                //Console.WriteLine("Please put your day:");
+                //var day = Console.ReadLine();
+                //Console.WriteLine("");
+                //Console.WriteLine(new Date(Convert.ToInt32(year), Convert.ToInt32(month), Convert.ToInt32(day)));
+
+                Employee employee1 = new SalaryEmployee()
+                {
+                    Id = 1,
+                    FirstName = "Pancho",
+                    LastName = "Villa",
+                    BirthDate = new Date(1945, 2, 20),
+                    HiringDate = new Date(2022, 1, 2),
+                    IsActive = true,
+                    Salary = 100000.55M
+                };
+
+                Console.WriteLine(employee1);
 
             }
             catch (Exception ex)
---
+++ MyFirstCodeOOP/MyFirstCodeOOP/SalaryEmployee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstCodeOOP
+{
+    public class SalaryEmployee : Employee
+    {
+        #region Properties
+        public decimal Salary { get; set; }
+        #endregion
+
+        #region Methods
+        public override decimal GetValueToPay()
+        {
+            return Salary;
+        }
+
+        public override string ToString()
+        {
+            //El base es la herencia que traigo de la clase padre, que para este caso se trata de una clase abstracta
+            return $"{base.ToString()} \n\t" +
+                $"Value to Pay; {GetValueToPay():C2}";
+        }
+        #endregion
+    }
+}
