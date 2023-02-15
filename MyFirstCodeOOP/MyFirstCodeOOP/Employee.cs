7265e7702321c3c6f8cb9b9281d9e1183b1dd103
Carlos Diaz <junior.diaz@selazar.com>
2023-02-15 18:45:22 -0500

    Added String.Format in Employee.cs

--- MyFirstCodeOOP/MyFirstCodeOOP/Employee.cs
+++ MyFirstCodeOOP/MyFirstCodeOOP/Employee.cs
@@ -20,13 +20,27 @@ namespace MyFirstCodeOOP
 
         public override string ToString()
         {
-            return $"------EMPLOYEE------\n\t" +
-                $"ID: {Id} \n\t " +
-                $"First Name: {FirstName} \n\t " +
-                $"Last Name: {LastName} \n\t " +
-                $"Birth date: {BirthDate} \n\t " +
-                $"Hiring Date: {HiringDate} \n\t " +
-                $"Is Active? {IsActive}";
+            return String.Format("--EMPLOYEE--\n\t" +
+                "Id: {0} \n\t" +
+                "Last Name: {2} \n\t" +
+                "First Name: {1} \n\t " +
+                "Birth Day: {3} \n\t " +
+                "Hiring Date: {4} \n\t " +
+                "Is Active? {5}",
+                Id,
+                FirstName,
+                LastName,
+                BirthDate,
+                HiringDate,
+                IsActive);
+
+            //return $"------EMPLOYEE------\n\t" +
+            //    $"ID: {Id} \n\t " +
+            //    $"First Name: {FirstName} \n\t " +
+            //    $"Last Name: {LastName} \n\t " +
+            //    $"Birth date: {BirthDate} \n\t " +
+            //    $"Hiring Date: {HiringDate} \n\t " +
+            //    $"Is Active? {IsActive}";
         }
 
         #endregion
