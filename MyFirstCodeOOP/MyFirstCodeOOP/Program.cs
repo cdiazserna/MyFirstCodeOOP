113725dcfc539d93e82b2453541a1e08edb403eb
Carlos Diaz <junior.diaz@selazar.com>
2023-02-15 19:32:58 -0500

    Improve the SalaryEmployee Object to get from screen the values

--- MyFirstCodeOOP/MyFirstCodeOOP/Program.cs
+++ MyFirstCodeOOP/MyFirstCodeOOP/Program.cs
@@ -12,28 +12,28 @@ namespace MyFirstCodeOOP
                 Console.WriteLine("OOP CONCEPTS");
                 Console.WriteLine("************");
 
-                Console.WriteLine("Please put your year:");
+                Console.WriteLine("Please put your birth date year:");
                 var year = Console.ReadLine();
                 Console.WriteLine("");
-                Console.WriteLine("Please put your month:");
+                Console.WriteLine("Please put your birth date month:");
                 var month = Console.ReadLine();
                 Console.WriteLine("");
-                Console.WriteLine("Please put your day:");
+                Console.WriteLine("Please put your birth date day:");
                 var day = Console.ReadLine();
                 Console.WriteLine("");
                 Console.WriteLine(new Date(Convert.ToInt32(year), Convert.ToInt32(month), Convert.ToInt32(day)));
-
+                Console.WriteLine("");
                 Console.WriteLine("******Testing latest implementation*******");
-
+                Console.WriteLine("");
                 Employee salaryEmployee = new SalaryEmployee()
                 {
-                    Id = 1000,
-                    FirstName = "Maria",
-                    LastName = "Posada",
-                    BirthDate = new Date(1950, 2, 5),
-                    HiringDate = new Date(2022, 12, 31),
-                    IsActive = true,
-                    Salary = 200000.34M                            
+                    Id = Convert.ToInt32(Console.ReadLine()),
+                    FirstName = Console.ReadLine(),
+                    LastName = Console.ReadLine(),
+                    BirthDate = new Date(Convert.ToInt32(year), Convert.ToInt32(month), Convert.ToInt32(day)),
+                    HiringDate = new Date(2022, 3, 4),
+                    IsActive = Convert.ToBoolean(Console.ReadLine()),
+                    Salary = Convert.ToDecimal(Console.ReadLine()),
                 };
 
                 Console.WriteLine(salaryEmployee);
